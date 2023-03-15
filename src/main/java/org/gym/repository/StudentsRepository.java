f82a3ca9d9adcb36d068bea3966066ee4f95219a
Salomao S <souza95salomao@gmail.com>
2023-03-15 00:51:18 +0000

    update: repositories import organization

--- src/main/java/org/gym/repository/StudentsRepository.java
+++ src/main/java/org/gym/repository/StudentsRepository.java
@@ -2,6 +2,7 @@ package org.gym.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+
 import org.gym.entity.Student;
 
 @Repository
